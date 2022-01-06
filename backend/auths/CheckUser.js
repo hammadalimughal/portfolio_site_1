9a21697d0e6a742b89cc658016c5fa0935838d7d
Hammad Ali <hammad19ali@gmail.com>
2022-01-07 04:48:26 +0500

    login authentication

--- backend/auths/CheckUser.js
+++ backend/auths/CheckUser.js
@@ -2,13 +2,20 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-router.post('/login', async (req,res) => {
-    let success = false;
-    const {name,email} = req.body;
+router.post('/checkuser', async (req,res) => {
+    var success = false;
+    const {email,password} = req.body;
     let user = await User.findOne({email})
-    if (!user) {
+    const passwordCompare = (user.password == password)
+    if (!user || !passwordCompare) {
         success = false;
         return res.status(400).json({ error: "Incorrect Credentials" })
     }
-
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+    res.json(data)
 })
+module.exports = router
--- backend/auths/RegisterUser.js
+++ backend/auths/RegisterUser.js
@@ -4,13 +4,20 @@ const router = express.Router();
 // const { body} = require('express-validator');
 
 router.post('/registeruser', async (req, res) => {
+    let userCheck = await User.findOne({email})
+    if(userCheck){
+        res.status(200).send("Some Error Occured")
+    }
+    // if(error){
+    //     console.error(error.message);
+    //     res.status(200).send("Some Error Occured")
+    // }
+    console.log("process")
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     })
     res.json({ user })
-    console.error(error.message);
-    res.status(200).send("Some Error Occured")
 })
 module.exports = router
